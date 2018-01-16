4d83f7fa878bdd894fb9489b4ae87f5e9cf0fcf7
Chris Warren <chris@ixalon.net>
2018-01-16 17:26:32 +0000

    Disable native2ascii

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,6 +23,7 @@ grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
 grails.project.war.file = "target/${appName}.war"
+grails.enable.native2ascii = false
 
 codenarc {
     reports = {
